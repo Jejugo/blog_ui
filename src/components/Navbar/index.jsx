2bbac3da30296fa7093391ef4d588b6b1c95d066
Nícolas Castellani Brisque <40432893+Cacatuaa@users.noreply.github.com>
2024-06-24 21:39:26 -0300

    feature/PB-2-create-navbar
    Criação inicial da navbar com alguns pontos pendentes, sendo eles:
    - Botão de Light/Dark mode;
    - Estilizar o texto do título e dos botões;

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,31 +1,33 @@
-import facebook from "@public/icons/facebook.png";
-import instagram from "@public/icons/instagram.png";
-import tiktok from "@public/icons/tiktok.png";
-import youtube from "@public/icons/youtube.png";
-import React from "react";
-import Image from "next/image";
-import * as S from "./Navbar.style";
+import facebook from "@public/icons/facebook.png"
+import instagram from "@public/icons/instagram.png"
+import tiktok from "@public/icons/tiktok.png"
+import youtube from "@public/icons/youtube.png"
+import Image from "next/image"
+import React from "react"
 
-const socialIcons = [{
-  name: "Facebook",
-  link: "https://www.facebook.com",
-  image: facebook
-},
-{
-  name: "Instagram",
-  link: "https://www.instagram.com",
-  image: instagram
-},
-{
-  name: "Tiktok",
-  link: "https://www.tiktok.com",
-  image: tiktok
-},
-{
-  name: "Youtube",
-  link: "https://www.youtube.com",
-  image: youtube
-}
+import * as S from "./Navbar.style"
+
+const socialIcons = [
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com",
+    image: facebook,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com",
+    image: instagram,
+  },
+  {
+    name: "Tiktok",
+    link: "https://www.tiktok.com",
+    image: tiktok,
+  },
+  {
+    name: "Youtube",
+    link: "https://www.youtube.com",
+    image: youtube,
+  },
 ]
 
 export default function Navbar() {
@@ -35,19 +37,13 @@ export default function Navbar() {
       <S.Icons>
         {socialIcons.map((socialIcon, index) => (
           <S.LinkIcons href={socialIcon.link} key={index}>
-            <Image
-              src={socialIcon.image}
-              alt={socialIcon.name}
-              fill
-            />
+            <Image src={socialIcon.image} alt={socialIcon.name} fill />
           </S.LinkIcons>
         ))}
       </S.Icons>
 
       {/* Div que contém o título */}
-      <S.Title>
-        blog-ui
-      </S.Title>
+      <S.Title>blog-ui</S.Title>
 
       {/* Div que contém os botões */}
       <S.Buttons>
@@ -57,5 +53,5 @@ export default function Navbar() {
         <S.LinkPages href="/home">Login</S.LinkPages>
       </S.Buttons>
     </S.Main>
-  );
+  )
 }
