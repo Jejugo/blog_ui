1d798366ee09c80182681edeea6e5051ab2681c9
Jeff Goes <goes.jeffjulian@gmail.com>
2024-06-26 00:14:29 -0300

    chore/PB-13 - Create Global CSS and Color Standards using ThemeProvider

---
+++ src/styles/fonts.js
@@ -0,0 +1,12 @@
+import { Inter } from "next/font/google";
+
+// We can import local fonts from next:
+// import localFont from "next/font/local";
+
+// define a custom local font where GreatVibes-Regular.ttf is stored in the styles folder
+// const greatVibes = localFont({ src: "./GreatVibes-Regular.ttf" });
+
+// define your variable fonts
+const inter = Inter({ subsets: ["latin"] });
+
+export { inter };
---
+++ src/styles/theme.js
@@ -0,0 +1,57 @@
+const commonStyles = {
+  spacings: {
+    small: "8px",
+    medium: "16px",
+    large: "24px",
+  },
+  fontSizes: {
+    small: "12px",
+    medium: "16px",
+    large: "24px",
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    small: "16px",
+    medium: "24px",
+    large: "32px",
+  },
+};
+
+export const lightTheme = {
+  colors: {
+    primary: "#0070f3",
+    background: "#ffffff",
+    secondaryBackground: "#f8f8f8",
+    text: "#000000",
+  },
+  button: {
+    primary: {
+      backgroundColor: "#ccc",
+      backgroundColorHover: "#2a2a2a",
+      color: "black",
+      colorHover: "white",
+    },
+  },
+  ...commonStyles,
+};
+
+export const darkTheme = {
+  colors: {
+    primary: "#0f172a",
+    background: "#0f172a",
+    secondaryBackground: "#010b22",
+    text: "#ffffff",
+  },
+  button: {
+    primary: {
+      backgroundColor: "#2b2652",
+      backgroundColorHover: "#ccc",
+      color: "white",
+      colorHover: "black",
+    },
+  },
+  ...commonStyles,
+};
