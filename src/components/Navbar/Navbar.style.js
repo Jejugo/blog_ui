e0ad3e51b7bf9f143aedf2b4c95bc57f6bd8a037
Nícolas Castellani Brisque <40432893+Cacatuaa@users.noreply.github.com>
2024-07-01 20:37:29 -0300

    Chore/PB-2 Applying prettier

--- src/components/Navbar/Navbar.style.js
+++ src/components/Navbar/Navbar.style.js
@@ -1,37 +1,35 @@
-import styled from "styled-components";
-import Link from "next/link";
+import Link from "next/link"
+import styled from "styled-components"
 
 export const Main = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 
 export const Icons = styled.div`
-    display: flex;
-    gap: 10px;
+  display: flex;
+  gap: 10px;
 `
 
 export const Title = styled.div`
-    font-size: ${({ theme }) => theme.fontSizes.large};
-    font-weight: ${({ theme }) => theme.fontWeights.bold};
+  font-size: ${({ theme }) => theme.fontSizes.large};
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
 `
 
-
 export const Buttons = styled.div`
-    display: flex;
-    gap: 10px;
+  display: flex;
+  gap: 10px;
 `
 
 export const LinkIcons = styled(Link)`
-    width: 20px;
-    height: 20px;
-    position: relative;
+  width: 20px;
+  height: 20px;
+  position: relative;
 `
 
 export const LinkPages = styled(Link)`
-    color: #000000;
-    font-size: ${({ theme }) => theme.fontSizes.medium};
-    font-weight: ${({ theme }) => theme.fontWeights.bold};
-
+  color: #000000;
+  font-size: ${({ theme }) => theme.fontSizes.medium};
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
 `
