1e3377177340d96dbc958ae45149f2c8bda0e0e3
Nícolas Castellani Brisque <40432893+Cacatuaa@users.noreply.github.com>
2024-06-30 21:11:01 -0300

    Chore/PB-2: Adjusting Navbar component
    Adjusting navbar component with the following changes:
    - Exporting the styled components to a separeted file;

---
+++ src/components/Navbar/Navbar.style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+export const Main = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const Icons = styled.div`
+    display: flex;
+    gap: 10px;
+`
+
+export const Title = styled.div`
+    font-size: ${({ theme }) => theme.fontSizes.large};
+    font-weight: ${({ theme }) => theme.fontWeights.bold};
+`
+
+
+export const Buttons = styled.div`
+    display: flex;
+    gap: 10px;
+`
+
+export const LinkIcons = styled(Link)`
+    width: 20px;
+    height: 20px;
+    position: relative;
+`
+
+export const LinkPages = styled(Link)`
+    color: #000000;
+    font-size: ${({ theme }) => theme.fontSizes.medium};
+    font-weight: ${({ theme }) => theme.fontWeights.bold};
+
+`
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,35 +1,10 @@
-import styled from "styled-components";
 import facebook from "@public/icons/facebook.png";
 import instagram from "@public/icons/instagram.png";
 import tiktok from "@public/icons/tiktok.png";
 import youtube from "@public/icons/youtube.png";
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
-
-const Main = styled.div`
-    display: flex;
-    justify-content: space-between;
-    font-size: 30px;
-    align-items: center;
-`
-
-const Icons = styled.div`
-    display: flex;
-    gap: 10px;
-    
-`
-
-const Buttons = styled.div`
-    display: flex;
-    gap: 10px;
-`
-
-const LinkStyle = styled(Link)`
-    width: 20px;
-    height: 20px;
-    position: relative;
-`
+import * as S from "./Navbar.style";
 
 const socialIcons = [{
   name: "Facebook",
@@ -55,32 +30,32 @@ const socialIcons = [{
 
 export default function Navbar() {
   return (
-    <Main>
+    <S.Main>
       {/* Div que contém os ícones */}
-      <Icons>
+      <S.Icons>
         {socialIcons.map((socialIcon, index) => (
-          <LinkStyle href={socialIcon.link} key={index}>
+          <S.LinkIcons href={socialIcon.link} key={index}>
             <Image
               src={socialIcon.image}
               alt={socialIcon.name}
               fill
             />
-          </LinkStyle>
+          </S.LinkIcons>
         ))}
-      </Icons>
+      </S.Icons>
 
       {/* Div que contém o título */}
-      <div>
+      <S.Title>
         blog-ui
-      </div>
+      </S.Title>
 
       {/* Div que contém os botões */}
-      <Buttons>
-        <Link href="/home">Home</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/about">About</Link>
-        <Link href="/home">Login</Link>
-      </Buttons>
-    </Main>
+      <S.Buttons>
+        <S.LinkPages href="/home">Home</S.LinkPages>
+        <S.LinkPages href="/contact">Contact</S.LinkPages>
+        <S.LinkPages href="/about">About</S.LinkPages>
+        <S.LinkPages href="/home">Login</S.LinkPages>
+      </S.Buttons>
+    </S.Main>
   );
 }
