812ff7532f171aa6cee93e43cee5d0e4e3cd79be
Gabriel <gabrielcfurlan@hotmail.com>
2024-07-13 14:17:42 -0300

    feature/PB-9-categories - adicionando imagens aos botoes

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,12 +1,36 @@
+import Image from "next/image"
+
 import * as S from "./Categories.style"
+import computer from "../../images/Computer.jpg"
+import fashionMan from "../../images/Fashion_Man.jpg"
+import kombi from "../../images/Kombi.jpg"
+import pasta from "../../images/Pasta.jpg"
 
 export const categories = [
-  "Fashion",
-  "Food",
-  "Coding",
-  "Style",
-  "Travel",
-  "Culture",
+  {
+    id: "Fashion",
+    image: <Image src={fashionMan} alt="Fashion Man" width={50} height={50} />,
+  },
+  {
+    id: "Food",
+    image: <Image src={pasta} alt="Pasta" width={50} height={50} />,
+  },
+  {
+    id: "Coding",
+    image: <Image src={computer} alt="computer" width={50} height={50} />,
+  },
+  {
+    id: "Style",
+    image: null,
+  },
+  {
+    id: "Travel",
+    image: <Image src={kombi} alt="kombi" width={50} height={50} />,
+  },
+  {
+    id: "Culture",
+    image: null,
+  },
 ]
 
 export default function Categories() {
