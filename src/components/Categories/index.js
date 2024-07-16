43e60f5b7f1af34c166fddfae27ae24af22f67e2
Gabriel <gabrielcfurlan@hotmail.com>
2024-07-15 22:44:40 -0300

    bugfix/PB-22-Fixing-most-popular-and-categories - Issues solved

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -13,37 +13,31 @@ export const categories = [
     id: 0,
     name: "Fashion",
     image: <Image src={fashionMan} alt="Fashion Man" width={50} height={50} />,
-    //color: "#FECCCC",
   },
   {
     id: 1,
     name: "Food",
     image: <Image src={pasta} alt="Pasta" width={50} height={50} />,
-    //color: "#D9FFC9",
   },
   {
     id: 2,
     name: "Coding",
     image: <Image src={computer} alt="computer" width={50} height={50} />,
-    //color: "#FFF2B9",
   },
   {
     id: 3,
     name: "Style",
     image: <Image src={sunGlasses} alt="sunGlasses" width={50} height={50} />,
-    //color: "#B9CEFF",
   },
   {
     id: 4,
     name: "Travel",
     image: <Image src={kombi} alt="kombi" width={50} height={50} />,
-    //color: "#FDCAE1",
   },
   {
     id: 5,
     name: "Culture",
     image: <Image src={world} alt="world" width={50} height={50} />,
-    //color: "#BBFDFF",
   },
 ]
 
