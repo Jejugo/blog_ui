a83655af980746cffa3d4f0eeb6f8bf76f2974a4
Gabriel <gabrielcfurlan@hotmail.com>
2024-07-30 20:52:03 -0300

    feature/PB-20-footer - Incrementos no Footer

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -1,5 +1,56 @@
-"use client"
-
+import Link from "next/link"
 import styled from "styled-components"
 
-export const FooterWrapper = styled.footer``
+export const FooterWrapper = styled.footer`
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 120px;
+  background-color: ${({ theme }) => theme.colors.secondary};
+`
+export const Description = styled.div``
+
+export const Blog = styled.div``
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+`
+
+export const Image = styled.div``
+
+export const Title = styled.div``
+
+export const SocialMedia = styled(Link)`
+  display: flex;
+  width: 20px;
+  height: 20px;
+`
+
+export const Links = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 80px;
+  margin: 0 auto;
+  padding: 0 20px;
+`
+
+export const LinkTitle = styled.div``
+
+export const LinkPages = styled(Link)`
+  display: flex;
+`
+
+export const BlogLinks = styled.div``
+
+export const CategorieLinks = styled.div``
+
+export const SocialLinks = styled.div``
+
+export const SocialIcons = styled.div`
+  display: flex;
+  gap: 10px;
+`
+
+export const Icons = styled(Link)``
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,86 @@
+"use client"
+
+import facebook from "@public/icons/facebook.png"
+import instagram from "@public/icons/instagram.png"
+import tiktok from "@public/icons/tiktok.png"
+import youtube from "@public/icons/youtube.png"
+import Image from "next/image"
+
 import * as S from "./Footer.style"
 
-function Footer() {
-  return <S.FooterWrapper>Footer</S.FooterWrapper>
-}
+const socialIcons = [
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com",
+    image: facebook,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com",
+    image: instagram,
+  },
+  {
+    name: "Tiktok",
+    link: "https://www.tiktok.com",
+    image: tiktok,
+  },
+  {
+    name: "Youtube",
+    link: "https://www.youtube.com",
+    image: youtube,
+  },
+]
 
-export default Footer
+export default function Footer() {
+  return (
+    <S.FooterWrapper>
+      <S.Blog>
+        <S.HeaderWrapper>
+          <S.Image>Imagem</S.Image>
+          <S.Title>Lamablog</S.Title>
+        </S.HeaderWrapper>
+        <S.Description>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          facilisis, nibh sed dictum egestas.
+        </S.Description>
+        <S.SocialIcons>
+          {socialIcons.map((socialIcon, index) => (
+            <S.Icons href={socialIcon.link} key={index}>
+              <Image
+                src={socialIcon.image}
+                alt={socialIcon.name}
+                height={20}
+                width={20}
+              />
+            </S.Icons>
+          ))}
+        </S.SocialIcons>
+      </S.Blog>
+
+      <S.Links>
+        <S.BlogLinks>
+          <S.LinkTitle>Link</S.LinkTitle>
+          <S.LinkPages href="/">Homepage</S.LinkPages>
+          <S.LinkPages href="/blog">Blog</S.LinkPages>
+          <S.LinkPages href="/about">About</S.LinkPages>
+          <S.LinkPages href="/contact">Contact</S.LinkPages>
+        </S.BlogLinks>
+        <S.CategorieLinks>
+          <S.LinkTitle>Tags</S.LinkTitle>
+          <S.LinkPages href="/style">Style</S.LinkPages>
+          <S.LinkPages href="/fashion">Fashion</S.LinkPages>
+          <S.LinkPages href="/coding">Coding</S.LinkPages>
+          <S.LinkPages href="/travel">Travel</S.LinkPages>
+        </S.CategorieLinks>
+        <S.SocialLinks>
+          <S.LinkTitle>Social</S.LinkTitle>
+          {socialIcons.map((socialIcon, index) => (
+            <S.SocialMedia href={socialIcon.link} key={index}>
+              {socialIcon.name}
+            </S.SocialMedia>
+          ))}
+        </S.SocialLinks>
+      </S.Links>
+    </S.FooterWrapper>
+  )
+}
