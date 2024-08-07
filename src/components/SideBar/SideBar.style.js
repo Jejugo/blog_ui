3f8fc2ea74d5b39b1302f776a96d811d852b70c1
Nícolas Castellani Brisque <40432893+Cacatuaa@users.noreply.github.com>
2024-08-07 20:31:46 -0300

    fix/PB-32 adjusted responsiveness

--- src/components/SideBar/SideBar.style.js
+++ src/components/SideBar/SideBar.style.js
@@ -2,6 +2,12 @@
 
 import styled from "styled-components"
 
+import breakpoint from "@/styles/breakpoints"
+
 export const SideBar = styled.div`
   flex: 2;
+
+  @media screen and (width < ${breakpoint.laptop}) {
+    display: none;
+  }
 `
