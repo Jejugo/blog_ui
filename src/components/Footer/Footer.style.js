d6b23c2404b41890195cf0c094dbacc8e3cd974a
GabrielFurlan <gabrielcfurlan@hotmail.com>
2024-08-07 21:18:13 -0300

    Feature/pb 20 footer (#17)
    * feature/PB-20-footer - Incrementos no Footer
    
    * feature/PB-20-footer - Ajustes finais no Footer
    
    * feature/PB-20-footer - Ajustes da descricao e espaçamento do Footer
    
    * feature/PB-20-footer - Adjusted so non empty styled ckl
    
    * feature/PB-20-footer - ajustes dos estilos e utilização do componente Flex

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -1,5 +1,20 @@
-"use client"
-
 import styled from "styled-components"
 
-export const FooterWrapper = styled.footer``
+export const FooterWrapper = styled.footer`
+  bottom: 0;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.primary};
+  padding-left: 100px;
+  padding-right: 100px;
+  margin-top: 100px;
+  padding-bottom: 20px;
+`
+
+export const Title = styled.div`
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  font-size: ${({ theme }) => theme.fontSizes.medium};
+`
+
+export const LinkTitle = styled.div`
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+`
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,107 @@
+"use client"
+
+import lama from "@images/llama_icon.png"
+import facebook from "@public/icons/facebook.png"
+import instagram from "@public/icons/instagram.png"
+import tiktok from "@public/icons/tiktok.png"
+import youtube from "@public/icons/youtube.png"
+import Image from "next/image"
+import Link from "next/link"
+
 import * as S from "./Footer.style"
+import Flex from "@/Components/Flex"
 
-function Footer() {
-  return <S.FooterWrapper>Footer</S.FooterWrapper>
+const socialIcons = [
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com",
+    image: facebook,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com",
+    image: instagram,
+  },
+  {
+    name: "Tiktok",
+    link: "https://www.tiktok.com",
+    image: tiktok,
+  },
+  {
+    name: "Youtube",
+    link: "https://www.youtube.com",
+    image: youtube,
+  },
+]
+
+export default function Footer() {
+  return (
+    <S.FooterWrapper>
+      <Flex gap="100px">
+        <Flex flex="3" direction="column" gap="15px">
+          <Flex gap={"20px"} alignItems={"center"}>
+            <Image
+              src={lama}
+              alt="Lama"
+              height={60}
+              width={60}
+              style={{ borderRadius: "50%" }}
+            />
+            <S.Title>Lamablog</S.Title>
+          </Flex>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            facilisis, nibh sed dictum egestas. Ut in lacus libero. Sed
+            efficitur odio id elit fringilla, non aliquam nisi elementum. Nulla
+            facilisi. Sed ultrices, nunc sit amet tincidunt tincidunt, nulla
+            justo ultrices nisi, quis tincidunt nunc orci at nibh. Nulla
+            facilisi. Nulla facilisi.
+          </div>
+          <Flex gap="10px">
+            {socialIcons.map((socialIcon, index) => (
+              <Link href={socialIcon.link} key={index}>
+                <Image
+                  src={socialIcon.image}
+                  alt={socialIcon.name}
+                  height={20}
+                  width={20}
+                />
+              </Link>
+            ))}
+          </Flex>
+        </Flex>
+
+        <Flex
+          flex="2"
+          justifyContent="space-between"
+          alignItems="center"
+          margin="0 auto"
+          padding="0 20px"
+        >
+          <Flex direction="column" gap="10px">
+            <S.LinkTitle>Link</S.LinkTitle>
+            <Link href="/">Homepage</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+          </Flex>
+          <Flex direction="column" gap="10px">
+            <S.LinkTitle>Tags</S.LinkTitle>
+            <Link href="/style">Style</Link>
+            <Link href="/fashion">Fashion</Link>
+            <Link href="/coding">Coding</Link>
+            <Link href="/travel">Travel</Link>
+          </Flex>
+          <Flex direction="column" gap="10px">
+            <S.LinkTitle>Social</S.LinkTitle>
+            {socialIcons.map((socialIcon, index) => (
+              <Link href={socialIcon.link} key={index}>
+                {socialIcon.name}
+              </Link>
+            ))}
+          </Flex>
+        </Flex>
+      </Flex>
+    </S.FooterWrapper>
+  )
 }
-
-export default Footer
