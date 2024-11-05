f5f1d32f70685e7c9f68cdffc447428ea3aa15ee
Jeff Goes <goes.jeffjulian@gmail.com>
2024-11-04 21:09:25 -0300

    Chore/Remove console warnings

--- src/components/Flex/Flex.style.js
+++ src/components/Flex/Flex.style.js
@@ -4,9 +4,9 @@ import styled from "styled-components"
 
 export const FlexStyle = styled.div`
   display: flex;
-  ${({ direction }) => direction && `flex-direction: ${direction}`};
-  ${({ justifyContent }) =>
-    justifyContent && `justify-content: ${justifyContent}`};
-  ${({ alignItems }) => alignItems && `align-items: ${alignItems}`};
-  ${({ gap }) => gap && `gap: ${gap}`};
+  ${({ $direction }) => $direction && `flex-direction: ${$direction}`};
+  ${({ $justifyContent }) =>
+    $justifyContent && `justify-content: ${$justifyContent}`};
+  ${({ $alignItems }) => $alignItems && `align-items: ${$alignItems}`};
+  ${({ $gap }) => $gap && `gap: ${$gap}`};
 `
